9c86a7fbc18789340fc71a67ae436d87fd2c23ac
mete96 <oguzwst3@gmail.com>
2022-02-09 16:06:48 +0300

    helal

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Services from "./components/Services";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Services />
+    </div>
+  );
+}
+
+export default App;
